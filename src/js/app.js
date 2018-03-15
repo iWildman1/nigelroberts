58aaaeb9fe6745a9242729fb6b2428e441f592af
Daniel Wildman <dan@Daniels-MacBook-Pro.local>
2018-03-15 15:08:05 +0000

    Loads of changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
 require('babel-polyfill'); //Allows use of ES2015+ code in transpiled file via a polyfill. Do not remove.
+
+import navigation from './navigation';
+
+(function() {
+    navigation.init();
+})();
